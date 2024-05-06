74600fd7390499c369b8fce31e6effafec9e187d
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-05-06 10:48:20 +0300

    Initial commit

---
+++ src/main/java/com/example/snakegame/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.snakegame.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String login;
+
+    private String email;
+
+    private String password;
+
+    private long rating;
+
+}
